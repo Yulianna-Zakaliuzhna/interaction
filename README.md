4f9ff1057732a929628a6244eb5e292e76499bf2
Yulianna-Zakaliuzhna <52538256+Yulianna-Zakaliuzhna@users.noreply.github.com>
2019-12-13 15:41:50 -0500

    Add files via upload
    working files added

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# interaction
-Interactive Design &amp; Development Re(Design)
+
+*logo*    
+
+  # INTERACTIVE DESIGN & DEVELOPMENT  
+*slogan* 
+
+*tech illustration/photography aligned to the right, next to the logo*
+
+*menu buttons aligned horizontally:* 
+
+      description employers tuition&grands apply
+
+
+***Alumni Case*** *each case floating on the opposite side*
+
+*first case starts on the right*
+
+    - headshot *aligned to the left* . visual peek on the portfolio pieces from behind, headshot-sized;  (automatic slides). bouncin/wiggling 
+
+    - proficiency in software, represented in icons, located below headshot.
+
+    - followed by name, styled quotation mark. 
+
+    - described student experience, how graduating IDD has helped in personal & career development. 
+
+*more button to load more alumni stories* ...
+
+***3 main content boxes***
+
+    WHY IDD, CAREER PATH, EDUCATION PATH
+
+  ***footer***
+
+- Visual Peek on Instagram: 3-4 boxes/ 1 row
+
+
+- social media icons: twitter, facebook, linkedin
+
+- legal info: privacy policy, contact, 
+
+
+- logo, copyright
+
+
+
+
+    
---
+++ js/javascript.js
@@ -0,0 +1,52 @@
+const usercard = [
+  {
+    id: 1,
+    name: `Nathan Morene`,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores officiis tempore nesciunt vero ad soluta adipisci explicabo ducimus fugiat, non rerum facere ipsa quaerat fuga numquam quos ex. Velit, cupiditate.`,
+    photo: `img/headshot-1.jpg`,
+  },
+  {
+    id: 2,
+    name: `Kaloh Dinnerware Set - Turquoise`,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores officiis tempore nesciunt vero ad soluta adipisci explicabo ducimus fugiat, non rerum facere ipsa quaerat fuga numquam quos ex. Velit, cupiditate.`,
+    photo: `img/headshot-2.jpg`,
+  },
+]
+
+
+function getUsercardAsHtmlString(usercard) {
+
+  return `<div class= "usercard" >   
+            <img class="headshot" src="${usercard.photo}" alt="${usercard.name}">
+              <ul>
+                <li class="h3"> ${usercard.name} </li>
+                <li class="h4"> ${usercard.description} </li>
+                
+              </ul>
+          
+           </div>`;
+}
+function renderUsercard(arr) {
+
+  //* 1. map() Array to a new Array
+  const arrOfHtml = arr.map(getUsercardAsHtmlString);
+
+  //* 2. join() Array into a String
+  const strOfHtml = arrOfHtml.join('\n');
+
+  //* 3. innerHTML String into Elements
+  document.getElementById('usercard').innerHTML = strOfHtml;
+
+$("#loadMore").click(() =>{
+  if (id != 10){
+     id += 5;
+     currentProducts = usercard.slice(0,id)
+     renderUsercard(currentUsercard);
+     }
+   if(id == 3){
+     $("#loadMore").css("visibility", "hidden");
+   }
+   })
+
+
+renderUsercard(usercard.slice(0,id));
